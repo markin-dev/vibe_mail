a59df4dff647ad6be45488d146e6af6c9a0a5474
Сергей Маркин <markin.dev@gmail.com>
2026-08-26 15:37:27 +0400

    clean up long class

--- admin-front/src/components/AppLayout/AppHeader.vue
+++ admin-front/src/components/AppLayout/AppHeader.vue
@@ -1,5 +1,11 @@
 <template>
-  <header class="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+  <header
+    :class="cn(
+      'flex h-(--header-height) shrink-0 items-center gap-2 border-b',
+      'transition-size',
+      $props.class,
+    )"
+  >
     <div class="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
       <SidebarTrigger class="-ml-1" />
       <Separator
@@ -26,7 +32,11 @@
 </template>
 
 <script setup lang="ts">
+import type { HTMLAttributes } from "vue"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
+import { cn } from "@/lib/utils"
+
+withDefaults(defineProps<{ class?: HTMLAttributes["class"] }>(), {})
 </script>
