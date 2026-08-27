2449d4af82a6e11e8db54d90f144747780631fa8
Сергей Маркин <markin.dev@gmail.com>
2026-08-27 11:15:27 +0400

    removed github link

--- admin-front/src/components/AppLayout/AppHeader.vue
+++ admin-front/src/components/AppLayout/AppHeader.vue
@@ -22,34 +22,12 @@
       <h1 class="text-base font-medium">
         Documents
       </h1>
-
-      <div class="ml-auto flex items-center gap-2">
-        <Button
-          variant="ghost"
-          size="sm"
-          class="
-            hidden
-            sm:flex
-          "
-          as-child
-        >
-          <a
-            href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
-            rel="noopener noreferrer"
-            target="_blank"
-            class="dark:text-foreground"
-          >
-            GitHub
-          </a>
-        </Button>
-      </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue';
-import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { cn } from '@/lib/utils';
